ce057746cc341bbb261c846210fa504793016794
edcooperai <ed@mybarber.ai>
2024-12-15 11:46:07 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 try {
   app.listen(PORT, () => {
